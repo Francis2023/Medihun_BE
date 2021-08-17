0d457c732f848e2b246d649b8954e2f0fe85f36f
Bola Inc <fngafua2023@gmail.com>
2021-08-17 02:33:37 -0600

    install firebase admin

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const app = require('./api/api.js');
+const admin = require('firebase-admin');
 
 const PORT = process.env.PORT || 8000
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`)
-});
+// app.listen(PORT, () => {
+//     console.log(`Server listening on ${PORT}`)
+// });
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server"});
-});
+// app.get("/api", (req, res) => {
+//     res.json({ message: "Hello from server"});
 
+// });
+
+admin.initializeApp({
+    credential: admin.credential.applicationDefault()
+});
+  
+const db = admin.firestore();
 app.timeout = 60 * 10 * 1000;
